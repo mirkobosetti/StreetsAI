cd92cd47f5ec1ab95fc6ec4a5540f9e8d04acc1d
Mirko Bosetti <mirkobosetti1998@gmail.com>
2025-10-14 12:47:15 +0200

    feat: enhance Polygon class with union and multiBreak methods; update World class to use roadBorders (me -> o.O)

--- src/primitives/polygon.ts
+++ src/primitives/polygon.ts
@@ -1,4 +1,4 @@
-import { getIntersection } from '../graph/utils'
+import { average, getIntersection, getRandomColor } from '../graph/utils'
 import type { drawOptions } from '../types'
 import Point from './point'
 import Segment from './segment'
@@ -16,22 +16,85 @@ class Polygon {
     }
   }
 
-  static break(poly1: Polygon, poly2: Polygon): Point[] {
+  static union(polys: Polygon[]): Segment[] {
+    Polygon.multiBreak(polys)
+    const keptSegs: Segment[] = []
+
+    for (let i = 0; i < polys.length; i++) {
+      for (const seg of polys[i].segments) {
+        let keep = true
+
+        for (let j = 0; j < polys.length; j++) {
+          if (i !== j) {
+            if (polys[j].containsSegment(seg)) {
+              keep = false
+              break
+            }
+          }
+        }
+
+        if (keep) {
+          keptSegs.push(seg)
+        }
+      }
+    }
+
+    return keptSegs
+  }
+
+  static break(poly1: Polygon, poly2: Polygon) {
     const segs1 = poly1.segments
     const segs2 = poly2.segments
-    const intersections: Point[] = []
 
     for (let i = 0; i < segs1.length; i++) {
       for (let j = 0; j < segs2.length; j++) {
         const int = getIntersection(segs1[i].p1, segs1[i].p2, segs2[j].p1, segs2[j].p2)
         if (int && int.offset != 1 && int.offset != 0) {
           const point = new Point(int.x, int.y)
-          intersections.push(point)
+
+          let aux = segs1[i].p2
+          segs1[i].p2 = point
+          segs1.splice(i + 1, 0, new Segment(point, aux))
+
+          aux = segs2[j].p2
+          segs2[j].p2 = point
+          segs2.splice(j + 1, 0, new Segment(point, aux))
         }
       }
     }
+  }
 
-    return intersections
+  static multiBreak(polygons: Polygon[]) {
+    for (let i = 0; i < polygons.length - 1; i++) {
+      for (let j = i + 1; j < polygons.length; j++) {
+        Polygon.break(polygons[i], polygons[j])
+      }
+    }
+  }
+
+  containsSegment(seg: Segment): boolean {
+    const midpoint = average(seg.p1, seg.p2)
+    return this.containsPoint(midpoint)
+  }
+
+  containsPoint(p: Point): boolean {
+    const outerPoint = new Point(Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)
+    let intersections = 0
+
+    for (const seg of this.segments) {
+      const int = getIntersection(p, outerPoint, seg.p1, seg.p2)
+      if (int) {
+        intersections++
+      }
+    }
+
+    return intersections % 2 == 1
+  }
+
+  drawSegments(ctx: CanvasRenderingContext2D) {
+    for (const seg of this.segments) {
+      seg.draw(ctx, { color: getRandomColor(), width: 5 })
+    }
   }
 
   draw(ctx: CanvasRenderingContext2D, options?: drawOptions) {
--- src/world.ts
+++ src/world.ts
@@ -1,40 +1,41 @@
 import type Graph from './graph/graph'
 import Envelope from './primitives/envelope'
-import type Point from './primitives/point'
 import Polygon from './primitives/polygon'
+import type Segment from './primitives/segment'
 
 class World {
   graph: Graph
   roadWidth: number
   roadRoundness: number
   envelopes: Envelope[]
-  intersections: Point[]
+  roadBorders: Segment[]
 
   constructor(graph: Graph, roadWidth = 80, roadRoundness = 3) {
     this.graph = graph
     this.roadWidth = roadWidth
     this.roadRoundness = roadRoundness
     this.envelopes = []
-    this.intersections = []
+    this.roadBorders = []
     this.generate()
   }
 
   generate() {
     this.envelopes.length = 0
 
-    this.graph.segments.forEach((segment) => {
+    for (const segment of this.graph.segments) {
       this.envelopes.push(new Envelope(segment, this.roadWidth, this.roadRoundness))
-    })
+    }
 
-    this.intersections = Polygon.break(this.envelopes[0].poly, this.envelopes[1].poly)
+    this.roadBorders = Polygon.union(this.envelopes.map((env) => env.poly))
   }
 
   draw(ctx: CanvasRenderingContext2D) {
     for (const env of this.envelopes) {
-      env.draw(ctx, { fill: '#BBB', stroke: '#BBB', lineWidth: 15 })
+      env.draw(ctx)
     }
-    for (const int of this.intersections) {
-      int.draw(ctx, { color: 'red', size: 6 })
+
+    for (const border of this.roadBorders) {
+      border.draw(ctx, { color: 'white', width: 4 })
     }
   }
 }
